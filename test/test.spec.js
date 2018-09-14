5f1dc543eb95b5279c9e55730296073962d04a11
Prateek <prateek.pronoc@gmail.com>
2018-09-14 15:29:54 +0530

    sanatied the input data for test

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,8 +5,8 @@ var indexify = require('../index');
 
 describe('#IndexifyArrayOfJSONObject', function() {
     it('should convert array to json object by property names', function() {
-        var result = indexify([{ id: 1, name: 'prateek' }, { id: 2, name: 'Faishal' }], 'id', 'name');
+        var result = indexify([{ id: 91, value: 'India' }, { id: 1, name: 'USA' }], 'id', 'value');
         console.log(result);
-        expect(result).to.deep.equal({ 1: 'prateek', 2: 'Faishal' });
+        expect(result).to.deep.equal({91:'India',1:'UDSA'});
     });
 });
